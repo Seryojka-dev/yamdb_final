3b82444d58eb1067a08da4dea884252c94eb0b7b
Сергей Колобов <seryonka88@gmail.com>
2022-09-11 21:43:11 +0300

    1.04

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,8 +1,8 @@
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
 from .views import UsersViewSet, retrieve_token, sign_up
-from django.contrib import admin
 
 router = SimpleRouter()
 
